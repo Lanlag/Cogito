5615ce94f09a57932c147d44b2c232a2af44f776
Lanlag <Uniego7@gmail.com>
2024-03-26 18:38:23 +0800

    Initial commit

---
+++ src/main/java/net/uniego/aida/cogito/CogitoClient.java
@@ -0,0 +1,10 @@
+package net.uniego.aida.cogito;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CogitoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/uniego/aida/cogito/CogitoMain.java
@@ -0,0 +1,10 @@
+package net.uniego.aida.cogito;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CogitoMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
